1f6229650eb99c591843f8d2d65cc1fa9f3af398
invisible_dreamer <invisible_dreamer@localhost>
2015-05-07 18:18:01 +0200

    Modified class IArtGallery.

--- src/model/interfaces/IArtGallery.java
+++ src/model/interfaces/IArtGallery.java
@@ -2,26 +2,47 @@ package model.interfaces;
 
 import java.util.List;
 
+import model.classes.Artwork;
+import model.classes.Exhibit;
+
 /**
- * Interface for the art gallery.
+ * Interface for the archive of the art gallery.
  * @author Elisa Casadio
  *
  */
 
 public interface IArtGallery {
+	
+	/**
+	 * Adds the artwork to the list of artworks that have been exhibited in the
+	 * art gallery.
+	 * 
+	 * @param artwork
+	 * 			the artwork.
+	 */
+	void addArtwork(final Artwork artwork);
+	
+	/**
+	 * Return the list of all artworks that have been exhibited in the art
+	 * gallery.
+	 * 
+	 * @return the list of artworks.
+	 */
+	List<Artwork> getArtwork();
 
 	/**
 	 * Adds the exhibit to the list of exhibits of the art gallery.
 	 * 
-	 * @param the exhibit.
+	 * @param exhibit
+	 * 			the exhibit.
 	 */
-	void addExhibit(final IExhibit exhibit);
+	void addExhibit(final Exhibit exhibit);
 	
 	/**
 	 * Return the list of all exhibits of the art gallery.
 	 * 
 	 * @return the list of exhibits.
 	 */
-	List<IExhibit> getExhibit();
+	List<Exhibit> getExhibit();
 	
 }
