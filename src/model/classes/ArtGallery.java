098e54e064592e7516e18ba4e027839e80c401f9
invisible_dreamer <invisible_dreamer@localhost>
2015-05-07 18:19:17 +0200

    Modified class ArtGallery and added serialization.

--- src/model/classes/ArtGallery.java
+++ src/model/classes/ArtGallery.java
@@ -1,34 +1,48 @@
 package model.classes;
 
-import java.util.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.interfaces.IArtGallery;
-import model.interfaces.IExhibit;
 
 /**
- * This class models a generic art gallery.
+ * This class models a generic archive of an art gallery.
  * @author Elisa Casadio
  *
  */
 
-public class ArtGallery implements IArtGallery {
+public class ArtGallery implements IArtGallery, Serializable {
 
-private final List<IExhibit> exhibits;
+	private static final long serialVersionUID = 1877939269484883678L;
+	private final List<Artwork> artworks;
+	private final List<Exhibit> exhibits;
 	
 	/**
 	 * Constructor.
 	 */
-	public ArtGallery(){
+	public ArtGallery() {
+		this.artworks = new ArrayList<>();
 		this.exhibits = new ArrayList<>();
 	}
 	
 	@Override
-	public void addExhibit(final IExhibit exhibit){
+	public void addArtwork(final Artwork artwork) {
+		this.artworks.add(artwork);
+	}
+	
+	@Override
+	public List<Artwork> getArtwork() {
+		return this.artworks;
+	}
+	
+	@Override
+	public void addExhibit(final Exhibit exhibit) {
 		this.exhibits.add(exhibit);
 	}
 	
 	@Override
-	public List<IExhibit> getExhibit(){
+	public List<Exhibit> getExhibit() {
 		return this.exhibits;
 	}
 
