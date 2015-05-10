ec27215ea989f03f16d6f6435d2e1bbd72620c2f
SofiaRos <SofiaRos@localhost>
2015-05-10 21:23:37 +0200

    Interfaces IBalance, ISalesManagement, ITicket

---
+++ src/model/interfaces/ITicket.java
@@ -0,0 +1,36 @@
+package model.interfaces;
+
+/**
+ * This interface models a ticket.
+ * @author Sofia Rosetti
+ *
+ */
+public interface ITicket {
+	
+	/**
+	 * This method returns the name of the exposition. 
+	 * 
+	 * @return the name of the related exposition
+	 */
+	String getExpName();
+	
+	/**
+	 * This method returns the price of the ticket. 
+	 * 
+	 * @return the price of the ticket
+	 */
+	double getPrice();
+	
+	/**
+	 * This method checks if the ticket is sold.
+	 * 
+	 * @return true, if the ticket is sold
+	 */
+	boolean isSold();
+	
+	/**
+	 * This method sets the field "sold" to true.
+	 */
+	void setSold();
+
+}
