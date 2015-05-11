59d8de86f17c5b51dee0b1ade9b77878ac9cccd6
invisible_dreamer <invisible_dreamer@localhost>
2015-05-11 17:43:05 +0200

    Modified some methods of class Exhibit and added new methods and new field.

--- src/model/classes/Exhibit.java
+++ src/model/classes/Exhibit.java
@@ -1,8 +1,10 @@
 package model.classes;
 
-import java.util.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
-import model.interfaces.IArtwork;
 import model.interfaces.IExhibit;
 
 /**
@@ -11,39 +13,44 @@ import model.interfaces.IExhibit;
  *
  */
 
-public class Exhibit implements IExhibit {
+public class Exhibit implements IExhibit, Serializable {
 
+	private static final long serialVersionUID = -717882106580253340L;
+	private static final int PRIME = 31;
+	
 	private final String titleExhibit;
 	private final String curator;
 	private final Calendar beginning;
 	private final Calendar end;
-	private final List<IArtwork> artworks;
+	private final List<Long> artworks;
 	private final double costExhibit;
+	private final double costTicket;
 	
 	/**
 	 * Constructor.
-	 * @param title
+	 * @param newTitle
 	 * 			the exhibit's title.
-	 * @param curator
+	 * @param newCurator
 	 * 			the curator's name of this exhibit.
-	 * @param beginning
+	 * @param newBeginning
 	 * 			the commencement date of this exhibit.
-	 * @param end
+	 * @param newEnd
 	 * 			the end date of this exhibit.
-	 * @param artworks
-	 * 			the list of artworks of this exhibit.
-	 * @param costExhibit
+	 * @param newCostExhibit
 	 * 			the cost of this exhibit.
+	 * @param newCostTicket
+	 * 			the cost of the ticket of this exhibit.
 	 */
-	public Exhibit(final String title, final String curator, 
-			final Calendar beginning, final Calendar end, 
-			final double costExhibit) {
-		this.titleExhibit = title;
-		this.curator = curator;
-		this.beginning = beginning;
-		this.end = end;
+	public Exhibit(final String newTitle, final String newCurator, 
+			final Calendar newBeginning, final Calendar newEnd, 
+			final double newCostExhibit, final double newCostTicket) {
+		this.titleExhibit = newTitle;
+		this.curator = newCurator;
+		this.beginning = newBeginning;
+		this.end = newEnd;
 		this.artworks = new ArrayList<>();
-		this.costExhibit = costExhibit;
+		this.costExhibit = newCostExhibit;
+		this.costTicket = newCostTicket;
 	}
 	
 	@Override
@@ -67,12 +74,12 @@ public class Exhibit implements IExhibit {
 	}
 	
 	@Override
-	public void addArtwork(final IArtwork artwork){
-		this.artworks.add(artwork);
+	public void addArtwork(final Long code) {
+		this.artworks.add(code);
 	}
 	
 	@Override
-	public List<IArtwork> getArtworks() {
+	public List<Long> getArtworks() {
 		return this.artworks;
 	}
 	
@@ -84,6 +91,88 @@ public class Exhibit implements IExhibit {
 	@Override
 	public double getCostExhibit() {
 		return this.costExhibit;
+	}
+	
+	@Override
+	public double getCostTicket() {
+		return this.costTicket;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = 1;
+		result = PRIME * result
+				+ ((artworks == null) ? 0 : artworks.hashCode());
+		result = PRIME * result
+				+ ((beginning == null) ? 0 : beginning.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(costExhibit);
+		result = PRIME * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(costTicket);
+		result = PRIME * result + (int) (temp ^ (temp >>> 32));
+		result = PRIME * result + ((curator == null) ? 0 : curator.hashCode());
+		result = PRIME * result + ((end == null) ? 0 : end.hashCode());
+		result = PRIME * result
+				+ ((titleExhibit == null) ? 0 : titleExhibit.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		final Exhibit other = (Exhibit) obj;
+		if (artworks == null) {
+			if (other.artworks != null) {
+				return false;
+			}
+		} else if (!artworks.equals(other.artworks)) {
+			return false;
+		}
+		if (beginning == null) {
+			if (other.beginning != null) {
+				return false;
+			}
+		} else if (!beginning.equals(other.beginning)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(costExhibit) != Double
+				.doubleToLongBits(other.costExhibit)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(costTicket) != Double
+				.doubleToLongBits(other.costTicket)) {
+			return false;
+		}
+		if (curator == null) {
+			if (other.curator != null) {
+				return false;
+			}
+		} else if (!curator.equals(other.curator)) {
+			return false;
+		}
+		if (end == null) {
+			if (other.end != null) {
+				return false;
+			}
+		} else if (!end.equals(other.end)) {
+			return false;
+		}
+		if (titleExhibit == null) {
+			if (other.titleExhibit != null) {
+				return false;
+			}
+		} else if (!titleExhibit.equals(other.titleExhibit)) {
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
