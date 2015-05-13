b6198d9c26362c585dd49abc930c09e59a638e3b
invisible_dreamer <invisible_dreamer@localhost>
2015-05-13 16:25:01 +0200

    Modified the javadoc's commet in the interface IControllerArtwork.

--- src/controller/interfaces/IControllerArtwork.java
+++ src/controller/interfaces/IControllerArtwork.java
@@ -10,15 +10,18 @@ import javax.swing.JFrame;
 public interface IControllerArtwork {
 
 	/**
-	 * Adds a new artwork.
+	 * Show a new empty form to add a new artwork to the archive of the artwork
+	 * of the art gallery. It also determines the code that must have the 
+	 * artwork in the archive.
 	 * 
 	 * @param frame
 	 * 			the frame of the view.
 	 */
-	void commandAdd(final JFrame frame);
+	void commandNew(final JFrame frame);
 	
 	/**
-	 * Changes the selected artwork.
+	 * Show a form setted with the informations of the selected artwork in the 
+	 * table to permit the modification of the informations.
 	 * 
 	 * @param index
 	 * 			the position of the artwork in the list.
@@ -28,7 +31,8 @@ public interface IControllerArtwork {
 	void commandEdit(final int index, final JFrame frame);
 	
 	/**
-	 * Shows the selected artwork.
+	 * Show a form setted with the informations of the selected artwork in the 
+	 * table to permit  the user to see the informations.
 	 * 
 	 * @param index
 	 * 			the position of the artwork in the list.
@@ -38,7 +42,9 @@ public interface IControllerArtwork {
 	void commandShow(final int index, final JFrame frame);
 	
 	/**
-	 * Deletes the selected artwork.
+	 * Deletes the selected artwork from the view and from the model by asking 
+	 * the user for confirmation. Show an error message if the artwork is or 
+	 * was exposed in an exhibit.
 	 * 
 	 * @param index
 	 * 			the position of the artwork in the list.
@@ -48,7 +54,8 @@ public interface IControllerArtwork {
 	void commandDelete(final int index, final JFrame frame);
 	
 	/**
-	 * Closes the frame and saves the model in to a file.
+	 * Closes the frame and saves the model in to a file. Show an error message
+	 * if the writing fails.
 	 * 
 	 * @param frame
 	 * 			the frame of the view.
