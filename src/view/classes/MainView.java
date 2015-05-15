efbf211d68395b7cd6ad55edcb013d8030b2ae90
invisible_dreamer <invisible_dreamer@localhost>
2015-05-15 18:33:13 +0200

    Created class MainView.

---
+++ src/view/classes/MainView.java
@@ -0,0 +1,16 @@
+package view.classes;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ * @author Sofia Rosetti
+ * @author Elisa Casadio
+ *
+ */
+
+public class MainView extends JFrame {
+
+	private static final long serialVersionUID = 6709954522924991930L;
+
+}
